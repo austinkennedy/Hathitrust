9fc01de4e8883b8ad65c7af8e59d6439c5fe79f0
Austin Kennedy <akenns1947@gmail.com>
2023-10-09 10:56:27 -0600

    pre-1750 analysis

--- code/categories.py
+++ code/categories.py
@@ -12,9 +12,19 @@ volume_weights = pd.read_csv("../temporary/topic_weights.csv")
 cross = pd.read_csv('../temporary/cross_topics.csv')
 topics = pd.read_csv('../input/20191007_keys.txt', sep = '\t', lineterminator='\n', header=None)
 
+volume_weights_pre1750 = pd.read_csv('../temporary/topic_weights_pre1750.csv')
+cross_pre1750 = pd.read_csv('../temporary/cross_topics_pre1750.csv')
+topics_pre1750 = pd.read_csv('../input/20230623_keys.txt', sep = '\t', lineterminator='\n', header=None)
+
 #fix topic numbers
 topics.drop(columns=0, inplace=True)
-topics['topic_number'] = list(range(1,61))
+topics_pre1750.drop(columns=0, inplace=True)
+
+topics['topic_number'] = list(range(1,len(topics)+1))
+topics_pre1750['topic_number'] = list(range(1, len(topics_pre1750) +1))
+
+print(topics)
+print(topics_pre1750)
 
 group = [5,9,22,26,35,46,50,55,60] #innocuous topics to be eliminated
 
@@ -28,13 +38,15 @@ def cross_share(data):
     share = df.sum(axis = 0) / sum(df.sum(axis=0)) #numerator: sum down across rows, denominator: sum the sum of rows to get total of all cross-topics
     return share
 
-def get_shares(top = topics, omit = None, length = 3):
+
+def get_shares(shares, top = topics, omit = None, length = 3):
     #'topics' is a list or dataframe of topics, where each row corresponds to a topic
     #'omit' is a list of topics to omit, should be a list of numbers
     #'length' is the size of the categories (i.e. how many topics should make up a category), default 3
 
     n = len(top) #get number of topics
-    topic_numbers = list(topics['topic_number']) # generate topic numbers
+    topic_numbers = list(top['topic_number']) # generate topic numbers
+    topic_dict = pd.Series(top[2].values, index = top.topic_number).to_dict() #mapping of topic numbers and words
 
     if omit is not None:
         topic_numbers = [i for i in topic_numbers if i not in omit] #remove innocuous topics
@@ -48,11 +60,13 @@ def get_shares(top = topics, omit = None, length = 3):
     cross_combos = [['x'.join(map(str, i)) for i in c] for c in cross_combos] #joins each topic pair with 'x' to reference 'shares'
     cross_shares = [[shares[str(i)] for i in c] for c in cross_combos] #get share for each element
     cross_sum = [sum(i) for i in cross_shares] #sum each row
+    topic_words = [[topic_dict[i] for i in t] for t in combos]
 
     #column names
     topic_names = ['topic' + str(i) for i in range(1, length+1)]
     cross_names = ['combination' + str(i) for i in range(1, comb(length, 2)+1)] #'math.comb' gives the number of combinations, not the combinations themselves
     share_names = ['share' + str(i) for i in range(1, comb(length, 2)+1)]
+    topic_words_names = ['words' + str(i) for i in range(1, length+1)]
 
     #convert to dataframes since each are lists of tuples, easier to join
     combos = pd.DataFrame(combos, columns=topic_names)
@@ -60,11 +74,13 @@ def get_shares(top = topics, omit = None, length = 3):
     cross_combos = pd.DataFrame(cross_combos, columns=cross_names)
     cross_shares = pd.DataFrame(cross_shares, columns=share_names)
     cross_sum = pd.DataFrame(cross_sum, columns=['Sum'])
+    topic_words = pd.DataFrame(topic_words, columns=topic_words_names)
 
 
-    tmp = pd.concat([combos, combo_sets, cross_combos, cross_shares, cross_sum], axis = 1)
+    tmp = pd.concat([combos, combo_sets, cross_combos, cross_shares, cross_sum, topic_words], axis = 1)
     df = pd.DataFrame(tmp)
     # df = pd.DataFrame(tmp, columns=[topic_names, cross_names, share_names, 'Sum'])
+    df.sort_values('Sum', ascending = False, inplace = True)
 
     return df
 
@@ -90,17 +106,26 @@ def distinct_categories(data):
     return(df)
 
 print('Calculating shares')
-shares = cross_share(cross)
+shares_all = cross_share(cross)
+shares_pre1750 = cross_share(cross_pre1750)
 
 print('Getting categories')
-clusters = get_shares(top = topics, omit = group, length = 3)
+clusters = get_shares(shares = shares_all, top = topics, omit = group, length = 3)
+clusters_pre1750 = get_shares(shares = shares_pre1750, top = topics_pre1750, length = 3)
+
+print('Exporting Categories')
+clusters.to_csv('../temporary/clusters.txt', index = False)
+clusters_pre1750.to_csv('../temporary/clusters_pre1750.txt', index = False)
 
 print('Finding distinct categories')
 clusters_corpus = distinct_categories(clusters)
+clusters_corpus_pre1750 = distinct_categories(clusters_pre1750)
 print(clusters_corpus)
+print(clusters_corpus_pre1750)
 
 print('Exporting Topics')
-topics.to_csv('../temporary/topics.csv', index=False)
+topics.to_csv('../temporary/topics.txt', index=False)
+topics_pre1750.to_csv('../temporary/topics_pre1750.txt', index=False)
 
 
 
--- code/cross_topics.py
+++ code/cross_topics.py
@@ -3,16 +3,21 @@ import pandas as pd
 import numpy as np
 import itertools
 
-
-#change wd to location of file
-os.chdir(os.path.dirname(__file__))
-
 #Import data
 data = pd.read_csv('../input/20191007_topics.txt', sep = '\t', lineterminator = '\n', header=None)
 data.drop(columns = 0, inplace = True)
 data[1] = [string[string.rfind('/UK_data/')+9:-4] for string in data[1]]
-data.columns = ['HTID'] + [i for i in range(1,61)]
+data.columns = ['HTID'] + [i for i in range(1,len(data.columns))]
 print('Data dimensions:' + str(data.shape))
+
+print(data.head)
+
+data_pre1750 = pd.read_csv('../input/20230921_infer_topics.txt', sep = '\t', lineterminator='\n', header=None)
+data_pre1750.drop(columns = 0, inplace = True)
+data_pre1750[1] = [string[string.rfind('/all/')+5:-4] for string in data_pre1750[1]]
+data_pre1750.columns = ['HTID'] + [i for i in range(1,len(data_pre1750.columns))]
+print('Data (Pre-1750) dimensions:' + str(data_pre1750.shape))
+print(data_pre1750.head)
 
 #Function for getting cross-topic weights
 def cross_multiply(df):
@@ -52,10 +57,15 @@ def cross_multiply(df):
 
 
 cross = cross_multiply(data)
+cross_pre1750 = cross_multiply(data_pre1750)
 
+print(cross.head)
+print(cross_pre1750.head)
 
 #export data
 print('exporting cross-topics')
 cross.to_csv('../temporary/cross_topics.csv', index = False)
+cross_pre1750.to_csv('../temporary/cross_topics_pre1750.csv', index = False)
 print('exporting topic weights')
 data.to_csv('../temporary/topic_weights.csv', index = False)
+data_pre1750.to_csv('../temporary/topic_weights_pre1750.csv', index = False)
