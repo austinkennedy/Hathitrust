0e0b8d81cf82997c62ac2315c68d5e4a5266e509
Austin Kennedy <akenns1947@gmail.com>
2023-10-17 13:00:32 -0600

    Minor Changes

--- code/shares.py
+++ code/shares.py
@@ -22,39 +22,39 @@ metadata['HTID'] = metadata.apply(fix_htid, axis=1)
 cross = pd.merge(cross, metadata, on='HTID', how='inner').drop(columns = ['oclc', 'Year'])
 print(cross.head())
 
-# #create sequence of years
-# years=[]
-# for year in range(1510,1891):
-#     years.append(year)
+#create sequence of years
+years=[]
+for year in range(1510,1891):
+    years.append(year)
 
 
-# def moving_shares(data, year):
-#     #get 20-year moving average of data
-#     df = data[(data['Year_rounded'] >= (year-10)) & (data['Year_rounded'] <= (year+10))] #grab volumes within +/- 10 year window
-#     df.drop(columns = 'Year_rounded', inplace=True)
+def moving_shares(data, year):
+    #get 20-year moving average of data
+    df = data[(data['Year_rounded'] >= (year-10)) & (data['Year_rounded'] <= (year+10))] #grab volumes within +/- 10 year window
+    df.drop(columns = 'Year_rounded', inplace=True)
 
-#     if 'HTID' in df.columns:
-#         df.drop(columns = ['HTID'], inplace=True)
+    if 'HTID' in df.columns:
+        df.drop(columns = ['HTID'], inplace=True)
 
-#     print(df)
+    print(df)
 
-#     share = df.sum(axis=0) / sum(df.sum(axis=0)) #numerator gets sum of each cross-topic across all volumes, denominator gets sum of all cross-topics over all volumes in window
+    share = df.sum(axis=0) / sum(df.sum(axis=0)) #numerator gets sum of each cross-topic across all volumes, denominator gets sum of all cross-topics over all volumes in window
 
-#     return share
+    return share
 
-# print('Calculating Moving Average Shares')
-# ct_shares = {}
-# for year in years:
-#     ct_shares[year] = moving_shares(cross, year)
+print('Calculating Moving Average Shares')
+ct_shares = {}
+for year in years:
+    ct_shares[year] = moving_shares(cross, year)
 
-# moving_average_shares = pd.DataFrame.from_dict(ct_shares)
+moving_average_shares = pd.DataFrame.from_dict(ct_shares)
 
-# print('Exporting data')
-# print(moving_average_shares.head())
-# moving_average_shares.to_csv('../temporary/moving_average_shares.csv', index=True)
+print('Exporting data')
+print(moving_average_shares.head())
+moving_average_shares.to_csv('../temporary/moving_average_shares.csv', index=True)
 
-# #export fixed metadata
-# metadata.to_csv('../temporary/metadata.csv')
+#export fixed metadata
+metadata.to_csv('../temporary/metadata.csv')
 
 
 
