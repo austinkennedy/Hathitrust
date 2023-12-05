fbce4ce9554600415c2fd8fbd8c8af95a4e164af
Austin Kennedy <akenns1947@gmail.com>
2023-12-05 13:02:05 -0700

    Fixed famous volumes plot

--- code/config.py
+++ code/config.py
@@ -1,47 +1,47 @@
 #raw volume data, with scores for each of the 60 topics
 #trained on full sample
-# topic_weights = 'full'
-
-# #topics themselves
-# topic_info = '../input/20191007_keys.txt'
-
-# #innocuous topics to be eliminated
-# eliminated_topics = [5,9,22,26,35,46,50,55,60]
-
-# #check before running 'topic_volume_weights.py'. The classification of the output from 'categories.py' into 'Religion', 'Science', and 'Political Economy' is subjective and needs to be manually classified.
-
-# categories = {
-#     'Religion':[4,12,52],
-#     'Science':[7,8,41],
-#     'Political Economy':[33,34,47]
-#     }
-
-# #set to 'False' to get triangle plots for every year instead of every half-century
-# half_century = True
-
-# output_folder = '../output/full_sample/'
-
-# ##Uncomment this section for Sample 1
-topic_weights = 'sample_1'
+topic_weights = 'full'
 
 #topics themselves
-topic_info = '../input/20231019_keys.txt'
+topic_info = '../input/20191007_keys.txt'
 
 #innocuous topics to be eliminated
-eliminated_topics = []
+eliminated_topics = [5,9,22,26,35,46,50,55,60]
 
 #check before running 'topic_volume_weights.py'. The classification of the output from 'categories.py' into 'Religion', 'Science', and 'Political Economy' is subjective and needs to be manually classified.
 
 categories = {
-    'Religion':[4,14,40],
-    'Science':[21,35,36],
-    'Political Economy':[33,52,57]
+    'Religion':[4,12,52],
+    'Science':[7,8,41],
+    'Political Economy':[33,34,47]
     }
 
 #set to 'False' to get triangle plots for every year instead of every half-century
 half_century = True
 
-output_folder = '../output/sample_1/'
+output_folder = '../output/full_sample/'
+
+# ##Uncomment this section for Sample 1
+# topic_weights = 'sample_1'
+
+# #topics themselves
+# topic_info = '../input/20231019_keys.txt'
+
+# #innocuous topics to be eliminated
+# eliminated_topics = []
+
+# #check before running 'topic_volume_weights.py'. The classification of the output from 'categories.py' into 'Religion', 'Science', and 'Political Economy' is subjective and needs to be manually classified.
+
+# categories = {
+#     'Religion':[4,14,40],
+#     'Science':[21,35,36],
+#     'Political Economy':[33,52,57]
+#     }
+
+# #set to 'False' to get triangle plots for every year instead of every half-century
+# half_century = True
+
+# output_folder = '../output/sample_1/'
 
 #####Uncomment for sample 2
 
--- code/famous_volumes.R
+++ code/famous_volumes.R
@@ -15,10 +15,11 @@ famous_selected <- famous_selected %>%
   group_by(Category) %>%
   mutate(shape = row_number())
 
-# famous_selected <- famous_selected %>%
-#   group_by(Category) %>%
-#   mutate(shape = row_number()) %>%
-#   arrange(Category, Year)
+famous_selected <- famous_selected %>%
+  group_by(Category) %>%
+  mutate(shape = row_number()) %>%
+  arrange(Category, Title) %>%
+  ungroup()
 
 color_map <- data.frame(Category = c('Religion', 'Political Economy', 'Science'),
                         color = c('blue', 'red', 'green4'))
@@ -29,21 +30,21 @@ label = seq(0,1,by=0.2)
 
 
 
-plot <- ggtern(famous_selected, aes(x = Political.Economy, y = Religion, z = Science, shape=Title, color = Title)) +
+plot <- ggtern(famous_selected, aes(x = Political.Economy, y = Religion, z = Science, shape = Title, color = Title)) +
   geom_point(size = 2, stroke = 1.2) +
-  scale_shape_manual(values = famous_selected$shape, labels = famous_selected$Title) +
-  scale_color_manual(values = famous_selected$color, labels = famous_selected$Title) +
+  scale_shape_manual(values = famous_selected$shape, labels = famous_selected$Title, breaks = famous_selected$Title) +
+  scale_color_manual(values = famous_selected$color, labels = famous_selected$Title, breaks = famous_selected$Title) +
 
   limit_tern(limits=c(0,1.0),
              breaks=seq(0,1,by=0.2),
              labels=label) +
   labs(x = 'Political\nEconomy', y = 'Religion', z = 'Science')+
-  theme_bw() +
+  theme_classic() +
   theme(tern.axis.title.R = element_text(hjust=0.6, vjust = 0.9, size = 10), tern.axis.title.L = element_text(hjust = 0.3, vjust = 0.9, size = 10),
         tern.axis.title.T = element_text(size = 10),
-        legend.title = element_text(size = 14, face = 'bold'), legend.text = element_text(size = 10), legend.spacing.y = unit(0.1, 'cm')) +
-  guides(color = guide_legend(byrow =TRUE, size =10),
-         shape = guide_legend(byrow=TRUE, size =10))
+        legend.title = element_text(size = 10, face = 'bold'), legend.text = element_text(size = 8), legend.spacing.y = unit(0.1, 'cm')) +
+  guides(color = guide_legend(byrow =TRUE, size =7),
+         shape = guide_legend(byrow=TRUE, size =7))
   
   
 
